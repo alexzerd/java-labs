f5934ade2a2c20c9a8242df2a7b2a486d5e09741
Aleksandra Kuznetsova <allthatkindoftrash@gmail.com>
2018-04-10 15:07:32 +0200

    Create ChatMessage.java

---
+++ Lab_8/entity/ChatMessage.java
@@ -0,0 +1,31 @@
+package entity;
+public class ChatMessage {
+	private String message;
+	private ChatUser author;
+	private long timestamp;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public ChatUser getAuthor() {
+		return author;
+	}
+	public void setAuthor(ChatUser author) {
+		this.author = author;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public ChatMessage(String message, ChatUser author, long timestamp) {
+		super();
+		this.message = message;
+		this.author = author;
+		this.timestamp = timestamp;
+	}
+	
+}
