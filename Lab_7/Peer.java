547a4633577980be83059d75d7b3808699e19590
Aleksandra Kuznetsova <allthatkindoftrash@gmail.com>
2018-04-10 15:05:49 +0200

    Create Peer.java

---
+++ Lab_7/Peer.java
@@ -0,0 +1,20 @@
+
+import java.net.InetSocketAddress;
+
+public class Peer {
+	private final String name;
+	private final InetSocketAddress address;
+
+	public Peer(String name, InetSocketAddress address) {
+		this.name = name;
+		this.address = address;
+
+	}
+	public InetSocketAddress getAddress() {
+		return address;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
