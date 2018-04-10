4a2ed360d2efe7c3e7580bf17f873239186014f7
Aleksandra Kuznetsova <allthatkindoftrash@gmail.com>
2018-04-10 15:08:06 +0200

    Create ChatUser.java

---
+++ Lab_8/entity/ChatUser.java
@@ -0,0 +1,33 @@
+package entity;
+public class ChatUser {
+	private String name;
+	private long lastInteractionTime;
+	private String sessionId;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getLastInteractionTime() {
+		return lastInteractionTime;
+	}
+	public void setLastInteractionTime(long lastInteractionTime) {
+		this.lastInteractionTime = lastInteractionTime;
+	}
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public ChatUser(String name, long lastInteractionTime, String sessionId) {
+		super();
+		this.name = name;
+		this.lastInteractionTime = lastInteractionTime;
+		this.sessionId = sessionId;
+	}
+}
